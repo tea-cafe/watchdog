a36baa13cdb7631b26176af0c93204ba2bec743a
sweifan <345627116@qq.com>
2017-12-12 20:51:43 +0800

    add config log path

--- ui/controllers/api/MediaList.php
+++ ui/controllers/api/MediaList.php
@@ -8,12 +8,13 @@ class MediaList extends BG_Controller {
         if (empty($this->arrUser)) {
             return $this->outJson('', ErrCode::ERR_NOT_LOGIN); 
         }
+        $media_name = $this->input->get('media_name', true);
         $account_id = $this->input->get('account_id', true);
         $check_status = $this->input->get('check_status', true);
         $pn = empty($this->input->get('currentPage')) ? 1 : intval($this->input->get('currentPage'));
         $rn = empty($this->input->get('pageSize')) ? 10 : intval($this->input->get('pageSize'));
         $this->load->model('MediaManager');
-        $arrData = $this->MediaManager->getMediaList($pn, $rn, $account_id, $check_status);
+        $arrData = $this->MediaManager->getMediaList($pn, $rn, $account_id, $check_status, $media_name);
         $this->outJson($arrData, ErrCode::OK);
     }
 
--- ui/controllers/chart/MediaData.php
+++ ui/controllers/chart/MediaData.php
@@ -1,5 +1,5 @@
 <?php
-class MediaData extends MY_Controller {
+class MediaData extends BG_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('chart/MediaDataModel');
