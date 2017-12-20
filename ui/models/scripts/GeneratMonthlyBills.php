e07c3c5ca2d783331e0fd54928e0266bfa884b43
tea-cafe <tea_cafe@163.com>
2017-12-20 18:50:46 +0800

    add ui/models/scripts/账单生成及回滚操作

--- ui/models/scripts/GeneratMonthlyBills.php
+++ ui/models/scripts/GeneratMonthlyBills.php
@@ -4,22 +4,75 @@
  *
  */
 class GeneratMonthlyBills extends CI_Model {
-        
+
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function do_execute() {
 
-        //$timeNow = time();
-        //$timeStart = mktime(0,0,0,date("m",$timeNow)-1,1,date("Y",$timeNow));
-        //$timeEnd = mktime(0,0,0,date("m",$timeNow),1,date("Y",$timeNow));
+        $this->load->database();
+
+        // 查询 monthly_action 获取月账单操作状态, 时间以上月1号0点时间戳为基准
+        $date = strtotime(date('Y-m-01') . ' -1 month');
+        $sqlCheck = 'SELECT action FROM monthly_action WHERE action_time=' . $date;
+        $objRes = $this->db->query($sqlCheck);
+        $arrErr = $this->db->error();
+        if ($arrErr['code'] !== 0) {
+            return [
+                'code' => -1,
+                'message' => 'Error : check monthly_action action ' . $arrErr['message'],
+            ];
+        }
+        $arrRes = $objRes->result_array();
+        if (!empty($arrRes)
+            && $arrRes[0]['action'] > 0) {
+            return [
+                'code' => 1,
+                'mssage' => 'ERROR: monthly bill for ' . date('Y-m', $date) . 'has generaged',
+            ];
+        }
+
         $dateStart =  date('Y-m-d', strtotime(date('Y-m-01') . ' -1 month'));
         $dateLast = date('Y-m-d', strtotime(date('Y-m-01') . ' -1 day'));
 
-        $sql = 'INSERT INTO monthly_bill(app_id,time,account_id,media_name,media_platform,create_time,money) (SELECT app_id,UNIX_TIMESTAMP(`date`),account_id,platform,media_name,UNIX_TIMESTAMP(`date`),SUM(post_profit) From tab_media_user_profit_sum_daily  WHERE `date` BETWEEN \'' . $dateStart . '\' AND \'' . $dateLast . '\' group by app_id)';
-        $this->load->database();
-        //$arrRes = $this->db->query('show tables');
-        //var_dump($arrRes->result_array());
-        $bolRes = $this->db->query($sql);
-        $error = $this->db->error();
-        echo '影响行数 : ' .  $this->db->affected_rows();
-        var_dump($bolRes, $error);
+
+        $sqlForMonthlyBill = 'INSERT INTO monthly_bill(app_id,time,account_id,media_name,media_platform,create_time,money) (SELECT app_id,UNIX_TIMESTAMP(`date`),account_id,platform,media_name,UNIX_TIMESTAMP(`date`),SUM(post_profit) From tab_media_user_profit_sum_daily  WHERE `date` BETWEEN \'' . $dateStart . '\' AND \'' . $dateLast . '\' group by app_id)';
+
+        // 更新monthly_action 为1
+        $sqlForMonthlyAction = 'INSERT INTO monthly_action(action_time, action) VALUES(' . $date . ',1) ON DUPLICATE KEY UPDATE action=VALUES(action)';
+
+        $this->db->trans_begin();
+        $this->db->query($sqlForMonthlyBill);
+        if ($this->db->affected_rows() === 0) {
+            $this->db->trans_rollback();
+            return [
+                'code' => 2,
+                'message' => '月账单' . date('Y-m', $date) . '插入失败，请检查monthly_bill',
+            ];
+        }
+        $this->db->query($sqlForMonthlyAction);
+        if ($this->db->affected_rows() === 0) {
+            $this->db->trans_rollback();
+            return [
+                'code' => -1,
+                'message' => '月账单' . date('Y-m', $date) . '操作记录插入失败，请检查monthly_bill',
+            ];
+            return;
+        }
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+            return [
+                'code' => -1,
+                'message' => '月账单生事务失败，请重试',
+            ];
+        } else {
+            $this->db->trans_commit();
+            return [
+                'code' => 0,
+                'message' => '月账单生成成功',
+            ];
+        }
+
     }
 }
--- ui/models/scripts/RollbackChannelBalance.php
+++ ui/models/scripts/RollbackChannelBalance.php
@@ -6,30 +6,83 @@
 class RollbackChannelBalance extends CI_Model {
         
     public function do_execute() {
+        $this->load->database();
 
+        // 查询 monthly_action 获取月账单操作状态, 时间以上月1号0点时间戳为基准
+        $date = strtotime(date('Y-m-01') . ' -1 month');
+        $sqlCheck = 'SELECT action FROM monthly_action WHERE action_time=' . $date;
+        $objRes = $this->db->query($sqlCheck);
+        $arrErr = $this->db->error();
+        if ($arrErr['code'] !== 0) {
+            return [
+                'code' => -1,
+                'message' => 'Error : check monthly_action action ' . $arrErr['message'],
+            ];
+        }
+        $arrRes = $objRes->result_array();
+        if (!empty($arrRes)
+            && $arrRes[0]['action'] != 2) {
+            return [
+                'code' => 1,
+                'message' => 'ERROR: ' . date('Y-m', $date) . '的账单未合入余额，请勿回滚',
+            ];
+        }
+            
         $timeNow = time();
         $timeStart = mktime(0,0,0,date("m",$timeNow)-1,1,date("Y",$timeNow));
         $timeEnd = mktime(0,0,0,date("m",$timeNow),1,date("Y",$timeNow));
 
-        $this->load->database();
-        $sql = 'SELECT account_id,create_time,SUM(money) as money From monthly_bill WHERE create_time>=0 group by account_id';
-        $arrRes = $this->db->query($sql);
+        $sqlMonthlyBill = 'SELECT account_id,create_time,SUM(money) as money From monthly_bill WHERE create_time>=0 group by account_id';
+        $arrRes = $this->db->query($sqlMonthlyBill);
         if ($this->db->error()['code'] !== 0) {
-            exit('chaxun monthly_bill shibai : ' . $this->db->error()['message']);
+            return [
+                'code' => -1,
+                'message' => 'ERROR : 月账单查询失败，' . $this->db->error()['message'],
+            ];
         }
 
-        $sql = 'INSERT INTO account_balance(account_id,update_time,money) VALUES'; 
+        $sqlForAccountBalance = 'INSERT INTO account_balance(account_id,update_time,money) VALUES'; 
         foreach ($arrRes->result_array() as $k => $val) {
-            $sql .= "('" . $val['account_id'] . "',"
+            $sqlForAccountBalance .= "('" . $val['account_id'] . "',"
                 . $val['create_time'] . ","
                 . $val['money'] . "),"; 
         }
-        $sql = substr($sql, 0, -1);
-        $sql .= ' ON DUPLICATE KEY UPDATE update_time=VALUES(create_time),money=money-VALUES(money)';
-        $arrRes = $this->db->query($sql);
-        if ($this->db->error()['code'] !== 0) {
-            exit('insert account_balance shibai');
+        $sqlForAccountBalance = substr($sqlForAccountBalance, 0, -1);
+        $sqlForAccountBalance .= ' ON DUPLICATE KEY UPDATE update_time=VALUES(create_time),money=money-VALUES(money)';
+
+        // 回滚 monthly_action 到 1 
+        $sqlForMonthlyAction = 'INSERT INTO monthly_action(action_time, action) VALUES(' . $date . ',1)  ON DUPLICATE KEY UPDATE action=VALUES(action)';
+
+        // 事务 start
+        $this->db->trans_begin();
+        $this->db->query($sqlForAccountBalance);
+        if ($this->db->affected_rows() === 0) {
+            $this->db->trans_rollback();
+            return [
+                'code' => 1,
+                'message' => '月账单' . date('Y-m', $date) . '回滚余额失败，请检查account_balance',
+            ];
         }
-        echo 'success';
+        $this->db->query($sqlForMonthlyAction);
+        if ($this->db->affected_rows() === 0) {
+            $this->db->trans_rollback();
+            return [
+                'code' => 1,
+                'message' => '月账单' . date('Y-m', $date) . '操作记录插入失败，请检查monthly_bill',
+            ];
+        }
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+            return [
+                'code' => -1,
+                'message' => '用户余额回滚事务失败，请重试',
+            ];
+        } else {
+            $this->db->trans_commit();
+            return [
+                'code' => 0,
+                'message' => '用户余额回滚成功',
+            ];
+        }
     }
 }
