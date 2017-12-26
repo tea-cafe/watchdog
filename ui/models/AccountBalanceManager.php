94084f581a5da980e3e5ce4d54bd43fdfd29211d
tea-cafe <tea_cafe@163.com>
2017-12-26 19:54:45 +0800

    fix bug for ui/models/AccountBalanceManager.php

--- ui/models/AccountBalanceManager.php
+++ ui/models/AccountBalanceManager.php
@@ -15,18 +15,37 @@ class AccountBalanceManager extends CI_Model {
         $timeStart = mktime(0,0,0,date("m",$timeNow)-1,1,date("Y",$timeNow));
         $timeEnd = mktime(0,0,0,date("m",$timeNow),1,date("Y",$timeNow));
 
+        $sqlSelectMonthlyAction = 'SELECT action from monthly_action WHERE action_time>=' . $timeStart;
+        $objMonthlyAction = $this->db->query($sqlSelectMonthlyAction);
+        if ($this->db->error()['code'] !== 0) {
+            ErrCode::$msg = '月账单操作信息失败';
+            return [];
+        }
+        $arrMonthlyAction = $objMonthlyAction->result_array();
+        if (empty($arrMonthlyAction[0]['action'])
+            || $arrMonthlyAction[0]['action'] == 0) {
+            ErrCode::$msg = '月账单还未生成';
+            return [];
+        }
+
         $sqlCount = 'SELECT COUNT(DISTINCT account_id) AS intcount From monthly_bill WHERE create_time>=' . $timeStart;
-        $arrCount = $this->db->query($sqlCount)->result_array();
+        $objCount = $this->db->query($sqlCount);
         if ($this->db->error()['code'] !== 0) {
             ErrCode::$msg = '月账单信息获取失败';
             return [];
         }
+        $arrCount = $objCount->result_array();
         $intCount = intval($arrCount[0]['intcount']);
 
         $sql = 'SELECT account_id,SUM(money) as money From monthly_bill WHERE create_time>=' . $timeStart . ' group by account_id limit ' . $rn*($pn-1) . ',' . $rn;
-        $arrProfitLastMonth = $this->db->query($sql)->result_array();
+        $objProfitLastMonth = $this->db->query($sql);
         if ($this->db->error()['code'] !== 0) {
             ErrCode::$msg = '月账单获取失败';
+            return [];
+        }
+        $arrProfitLastMonth = $objProfitLastMonth->result_array();
+        if (empty($arrProfitLastMonth)) {
+            ErrCode::$msg = '请确认月账单是否已生成';
             return [];
         }
         $strSqlIn = '(';
@@ -36,23 +55,25 @@ class AccountBalanceManager extends CI_Model {
         $strSqlIn = substr($strSqlIn, 0, -1) . ')';
 
         $strSqlSelectAccountBalance = 'SELECT account_id,money FROM account_balance where account_id in ' . $strSqlIn;
-        $arrPreAccountBalance = $this->db->query($sql)->result_array();
+
+        $objPreAccountBalance = $this->db->query($strSqlSelectAccountBalance);
         if ($this->db->error()['code'] !== 0) {
             ErrCode::$msg = '账户余额获取失败';
             return [];
         }
+        $arrPreAccountBalance = $objPreAccountBalance->result_array();
         $arrFormatAccountBalance = [];
         foreach ($arrPreAccountBalance as $val) {
             $arrFormatAccountBalance[$val['account_id']] = $val['money'];
         }
 
         $strSqlSelectEmail = 'SELECT account_id,email FROM account_info WHERE account_id IN ' . $strSqlIn;
-        $arrEmails = $this->db->query($strSqlSelectEmail)->result_array();
-        if ($this->db->error()['code'] !== 0
-        || empty($arrEmails)) {
+        $objEmails = $this->db->query($strSqlSelectEmail);
+        if ($this->db->error()['code'] !== 0) {
             ErrCode::$msg = '用户信息获取失败';
             return [];
         }
+        $arrEmails = $objEmails->result_array();
         $arrFormatEmails = [];
         foreach($arrEmails as $arrEmail) {
             $arrFormatEmails[$arrEmail['account_id']] = $arrEmail['email'];
@@ -64,11 +85,16 @@ class AccountBalanceManager extends CI_Model {
             $arrTmp['account_id'] = $arrBillList['account_id'];
             $arrTmp['email'] = $arrFormatEmails[$arrBillList['account_id']];
             if (empty($arrFormatAccountBalance[$arrBillList['account_id']])) {
-                $arrTmp['pre_account'] = 0;
+                $arrTmp['pre_balance'] = 0;
             } else {
-                $arrTmp['pre_account'] = intval($arrFormatAccountBalance[$arrBillList['account_id']]);
+                $arrTmp['pre_balance'] = $arrFormatAccountBalance[$arrBillList['account_id']];
             }
-            $arrTmp['post_balance'] = $arrTmp['pre_account'] + intval($arrBillList['money']);
+            if ($arrMonthlyAction[0]['action'] == 1) {
+                $arrTmp['post_balance'] = $arrTmp['pre_balance'] + $arrBillList['money'];
+            } else {
+                $arrTmp['post_balance'] = '-';
+            }
+            $arrTmp['period'] = date('Y-m', $timeStart); 
             $arrRes[] = $arrTmp;
             $arrTmp = [];
         }
--- ui/models/BillManager.php
+++ ui/models/BillManager.php
@@ -26,7 +26,7 @@ class BillManager extends CI_Model {
             return [];
         }
 
-        $strSqlSelectMediaName = 'SELECT app_id,media_name FROM media_info WHERE account_id=\'' . $strAccountId . "'";
+        $strSqlSelectMediaName = 'SELECT app_id,media_name,media_platform FROM media_info WHERE account_id=\'' . $strAccountId . "'";
         $arrMediaNames = $this->db->query($strSqlSelectMediaName)->result_array();
         if ($this->db->error()['code'] !== 0) {
             ErrCode::$msg = '月账单信息获取失败';
@@ -34,14 +34,16 @@ class BillManager extends CI_Model {
         }
         $arrFormatMediaNames = [];
         foreach($arrMediaNames as $arrMediaName) {
-            $arrFormatMediaNames[$arrMediaName['app_id']] = $arrMediaName['media_name'];
+            $arrFormatMediaNames[$arrMediaName['app_id']]['media_name'] = $arrMediaName['media_name'];
+            $arrFormatMediaNames[$arrMediaName['app_id']]['media_platform'] = $arrMediaName['media_platform'];
         }
 
         $arrRes = [];
         foreach ($arrAppMoney as $arrMoney) {
             $arrRes[] = [
-                'media_name' => $arrFormatMediaNames[$arrMoney['app_id']],
-                'date' => date('Y-m', $timeStart), 
+                'media_name' => $arrFormatMediaNames[$arrMoney['app_id']]['media_name'],
+                'media_platform' => $arrFormatMediaNames[$arrMoney['app_id']]['media_platform'],
+                'period' => date('Y-m', $timeStart), 
                 'money' => $arrMoney['money'],
             ];
         }
